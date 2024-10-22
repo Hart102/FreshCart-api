5f5a2c95008bc4b11af6646d30a950f1c2d68fbe
Hart C Justman <hartongit102@gmail.com>
2024-10-22 10:08:02 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/user", userRouter)
-app.use("/api/product", productRouter)
+app.use("/api/products", productRouter)
 app.use("/api/category", categoryRouter)
 app.use("/api/cart", cartRouter)
 
